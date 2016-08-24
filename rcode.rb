ccb1cb8939948611398ea0b1782f3a6680bdf522
Ben Allen <benjaminallen1982@gmail.com>
2016-08-24 17:08:30 +0100

    Emails added

--- rcode.rb
+++ rcode.rb
@@ -9,9 +9,20 @@ class Person
 	@first_name = name.capitalize
 	@surname = sname.capitalize
 	@fullname = @first_name + " " + @surname
+	@emails = []
 	end
 
 	def date
 		Date.parse(@dob).strftime("%Y-%d-%m")
 	end
+
+
+	def emails
+		@emails
+	end
+
+	def add_emails(email)
+		@emails.push(email)
+	end
+
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,11 +2,21 @@ require 'help'
 
 describe "Person class - entry in phonebook" do
   it "should store and return personal information" do
-    
+ 
     person = Person.new("ben", "allen", "2 Dec 1982")
 	expect(person.date.to_s).to eq "1982-02-12"
     expect(person.first_name).to eq "Ben"
     expect(person.surname).to eq "Allen"
     expect(person.fullname).to eq "Ben Allen"
-  end		
+  end	
+end
+
+describe "Emails method - entry in email"do
+	it "should store new emails that are added"do
+  
+	person = Person.new("ben", "allen", "2 Dec 1982")
+	person.add_emails("benallen66@hotmail.com")
+	# person.add_emails("benjaminallen1982@gmail.com")
+	expect(person.emails).to eq ["benallen66@hotmail.com"]
+  end
 end
